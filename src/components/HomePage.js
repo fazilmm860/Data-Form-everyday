84a27058d55876ca1581299541e314e7f6528786
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-18 11:12:00 +0530

    Homepage alignment edited

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,7 +18,7 @@ const title = [
         to: "/form"
     },
     {
-        choose: "ICICI",
+        choose: "SBI",
         to: "/form"
     },
 ]
@@ -30,7 +30,7 @@ export default function HomePage() {
             <div className="grid gap-1 grid-cols-1 md:grid-cols-5 mt-5 ">
                 {title.map((item, index) => (
                     <Link to={item.to}>
-                        <div key={index} className="flex flex-col w-60 h-full ml-2 items-center justify-center rounded-lg bg-gray-300 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+                        <div key={index} className="flex flex-col w-60 h-full ml-2 items-center justify-center rounded-lg bg-gray-300 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  hover:bg-gray-500 dark:bg-neutral-700 md:max-w-xl md:flex-row">
                             <div className="flex flex-col justify-center p-6">
                                 <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50 text-center">
                                     {item.choose}
